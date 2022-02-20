97646d8de801747094e4083fe25b03f7c40d5bd2
Scott Kloustin <532080+klous@users.noreply.github.com>
2022-02-20 15:36:23 -0500

    converting to use double instead of BigDecimal math functions

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -3,38 +3,51 @@ import java.math.RoundingMode;
 
 public class Loan {
     private int termInMonths;
-
     public int getTermInMonths() {return termInMonths;}
 
-    private BigDecimal loanAmount;
-    public BigDecimal getLoanAmount() {return loanAmount;}
+//    private BigDecimal loanAmount;
+//    public BigDecimal getLoanAmount() {return loanAmount;}
 
-
-    private BigDecimal interestRate;
-    public BigDecimal getInterestRate() {return interestRate;}
-
-
-
-    public BigDecimal getPayment(){
-        BigDecimal payment = BigDecimal.ZERO;
-        payment.setScale(2, RoundingMode.HALF_UP);
-        //if loan amount was 100000, interest rate 7.5%, term in months = 360
-        // payment = 100000*(0.05/12)*(1+0.05/12)^360   /    (1+0.075/12)^360 - 1)
-        BigDecimal monthlyRate = interestRate.divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
-
-        BigDecimal intermediateStep = BigDecimal.ZERO;
-        intermediateStep.setScale(2, RoundingMode.HALF_UP);
-
-        intermediateStep = (loanAmount.multiply(monthlyRate.multiply(monthlyRate.add(BigDecimal.ONE)).pow(termInMonths)));
-        // 100,000 with 5% interest rate shoudl be 22,338.72 at this point
-
-        payment = (loanAmount.multiply(monthlyRate.multiply(monthlyRate.add(BigDecimal.ONE)).pow(termInMonths))).divide
-                ((monthlyRate.add(BigDecimal.ONE)).pow(termInMonths-1));
-
-        return payment;
+    private double loanAmount;
+    public double getLoanAmount(){
+        return loanAmount;
     }
 
-    public Loan(int termInMonths, BigDecimal loanAmount, BigDecimal interestRate){
+//    private BigDecimal interestRate;
+//    public BigDecimal getInterestRate() {return interestRate;}
+
+    private double interestRate;
+    public double getInterestRate() {return interestRate;}
+
+    public double getPayment(){
+        double loanPayment = 0;
+        // payment = 100000*(0.05/12)*(1+0.05/12)^360   /    (1+0.075/12)^360 - 1)
+        double monthlyInterestRate = interestRate / 12;
+        loanPayment = (loanAmount*(monthlyInterestRate)*Math.pow((1+monthlyInterestRate),termInMonths)) / (Math.pow((1+monthlyInterestRate), termInMonths-1));
+
+        return loanPayment;
+    }
+
+//    public BigDecimal getPayment(){
+//        BigDecimal payment = BigDecimal.ZERO;
+//        payment.setScale(2, RoundingMode.HALF_UP);
+//        //if loan amount was 100000, interest rate 7.5%, term in months = 360
+//        // payment = 100000*(0.05/12)*(1+0.05/12)^360   /    (1+0.075/12)^360 - 1)
+//        BigDecimal monthlyRate = interestRate.divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
+//
+//        BigDecimal intermediateStep = BigDecimal.ZERO;
+//        intermediateStep.setScale(2, RoundingMode.HALF_UP);
+//
+//        intermediateStep = (loanAmount.multiply(monthlyRate.multiply(monthlyRate.add(BigDecimal.ONE)).pow(termInMonths)));
+//        // 100,000 with 5% interest rate shoudl be 22,338.72 at this point
+//
+//        payment = (loanAmount.multiply(monthlyRate.multiply(monthlyRate.add(BigDecimal.ONE)).pow(termInMonths))).divide
+//                ((monthlyRate.add(BigDecimal.ONE)).pow(termInMonths-1));
+//
+//        return payment;
+//    }
+
+    public Loan(int termInMonths, double loanAmount, double interestRate){
         this.termInMonths = termInMonths;
         this.loanAmount = loanAmount;
         this.interestRate = interestRate;
--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -4,7 +4,9 @@ public class MainProgram {
     public static void main(String[] args) {
         System.out.println("Main program output here");
 
-        Loan l = new Loan(360, new BigDecimal(100000), new BigDecimal(0.05));
+        //Loan l = new Loan(360, new BigDecimal(100000), new BigDecimal(0.05));
+
+        Loan l = new Loan(360, 100000, 0.05);
 
         System.out.println("Loan Payment is: " + l.getPayment());
 
