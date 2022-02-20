7c2fdb09dc083d42b8a99301892bd9dee55dde8d
Scott Kloustin <532080+klous@users.noreply.github.com>
2022-02-20 17:35:23 -0500

    working calculations and rounding

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -5,28 +5,53 @@ public class Loan {
     private int termInMonths;
     public int getTermInMonths() {return termInMonths;}
 
-//    private BigDecimal loanAmount;
-//    public BigDecimal getLoanAmount() {return loanAmount;}
 
     private double loanAmount;
     public double getLoanAmount(){
         return loanAmount;
     }
 
-//    private BigDecimal interestRate;
-//    public BigDecimal getInterestRate() {return interestRate;}
-
+    /**
+     * Use numbers like 5.75 is equivalent to 5.75%
+     */
     private double interestRate;
     public double getInterestRate() {return interestRate;}
 
+    private double getInterestPayment(double principalBalance){
+        double monthlyInterest = principalBalance * ((interestRate / 100) / 12);
+        return round(monthlyInterest, 2);
+    }
+
+    public double getPerDiemInterest(){
+        double perDiemInterest = loanAmount * (interestRate/100/365);
+        return round(perDiemInterest, 2);
+    }
+
+    public double getInterestOnlyPayment(){
+        return round(getInterestPayment(loanAmount), 2);
+    }
+
+
+    // method that rounds using BigDecimal
+    private double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+
     public double calculatePayment(){
-        double loanPayment = 0;
+        double interestRateDecimal = interestRate / 100;
         // payment = 100000*(0.05/12)*(1+0.05/12)^360   /    (1+0.075/12)^360 - 1)
         // should be 536.82
-        double monthlyInterestRate = interestRate / 12;
-        loanPayment = (monthlyInterestRate * loanAmount) / (1-Math.pow(1+monthlyInterestRate, -termInMonths));
-        return loanPayment;
+        double monthlyInterestRate = interestRateDecimal / 12;
+        double loanPayment = (monthlyInterestRate * loanAmount) / (1-Math.pow(1+monthlyInterestRate, -termInMonths));
+        return round(loanPayment, 2);
     }
+
+
 
 //    public BigDecimal getPayment(){
 //        BigDecimal payment = BigDecimal.ZERO;
@@ -47,7 +72,14 @@ public class Loan {
 //        return payment;
 //    }
 
-    public Loan(int termInMonths, double loanAmount, double interestRate){
+    /**
+     *
+     * @param termInMonths
+     * @param loanAmount represent the initial loan amount / principal amount
+     * @param interestRate use numbers like 5.75 = 5.75%
+     */
+
+    public Loan(double loanAmount, double interestRate, int termInMonths){
         this.termInMonths = termInMonths;
         this.loanAmount = loanAmount;
         this.interestRate = interestRate;
--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,14 +1,14 @@
-import java.math.BigDecimal;
-
 public class MainProgram {
     public static void main(String[] args) {
         System.out.println("Main program output here");
 
         //Loan l = new Loan(360, new BigDecimal(100000), new BigDecimal(0.05));
 
-        Loan l = new Loan(360, 100000, 0.05);
+        Loan l = new Loan( 100000, 5, 360);
 
         System.out.println("Loan Payment is: " + l.calculatePayment());
+        System.out.println("Your monthly interest (Interest Only) is: " + l.getInterestOnlyPayment());
+        System.out.println("Your per diem interest is: " + l.getPerDiemInterest());
 
     }
 }
